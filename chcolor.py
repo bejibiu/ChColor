ebda40bafcf16e465fa1a7f3b990d8c8d587a708
6ejibiu <akimovpnz@live.com>
2020-01-04 00:59:02 +0300

    add change color in svg

--- chcolor.py
+++ chcolor.py
@@ -4,29 +4,47 @@ import sys
 
 from PIL import Image
 
-PATH = '.'
+PATH = './img/'
 LEVEL = logging.WARNING
-NEXT_COLOR = [255, 255, 255]
+NEXT_COLOR = [255, 255, 255]  # #D41920
 
 
 def chCollor(PATH, NEXT_COLOR):
     pngs = [f for f in os.listdir(PATH) if f.endswith('png')]
-    logging.info('Fount %d images' % len(pngs))
+    svgs = [f for f in os.listdir(PATH) if f.endswith('svg')]
+    logging.info('Fount %d images' % (len(pngs)+len(svgs)))
+    if not os.path.exists(os.path.join(PATH, 'out')):
+        os.makedirs(os.path.join(PATH, 'out'))
     for png in pngs:
-        logging.debug('Change %s images' % png)
-        img = Image.open(os.path.join(PATH, png))
-        k, j = img.size
-        # img.show()
-        for i in range(k):
-            for ii in range(j):
-                if not img.getpixel((i, ii))[3] == 0:
-                    alpha = img.getpixel((i, ii))[3]
-                    img.putpixel((i, ii), (NEXT_COLOR[0], NEXT_COLOR[1],
-                                           NEXT_COLOR[2], alpha))
-        if not os.path.exists(os.path.join(PATH, 'out')):
-            os.makedirs(os.path.join(PATH, 'out'))
-        img.save(os.path.join(PATH, 'out', png))
-        logging.info('save {}'.format(png))
+        chColorPng(png, PATH, NEXT_COLOR)
+    for svg in svgs:
+        chColorSvg(svg, PATH, NEXT_COLOR)
+
+
+def chColorPng(png, PATH, NEXT_COLOR):
+    logging.debug('Change %s images' % png)
+    img = Image.open(os.path.join(PATH, png))
+    k, j = img.size
+    # img.show()
+    for i in range(k):
+        for ii in range(j):
+            if not img.getpixel((i, ii))[3] == 0:
+                alpha = img.getpixel((i, ii))[3]
+                img.putpixel((i, ii), (NEXT_COLOR[0], NEXT_COLOR[1],
+                                       NEXT_COLOR[2], alpha))
+    img.save(os.path.join(PATH, 'out', png))
+    logging.info('save {}'.format(png))
+
+
+def chColorSvg(svg, PATH, NEXT_COLOR):
+    logging.debug('Change %s images' % svg)
+    with open(os.path.join(PATH, svg), 'r') as img_file:
+        img = img_file.read()
+    next_color_hex = "".join([f"{i:x}" for i in NEXT_COLOR])
+    new_img = img.replace('/>', f' fill="#{next_color_hex}" />')
+    with open(os.path.join(PATH, 'out', svg), 'w') as img_save_file:
+        img_save_file.write(new_img)
+    logging.info('save {}'.format(svg))
 
 
 def split_list_from_collor(color):
